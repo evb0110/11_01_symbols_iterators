d83bd51cb882f5ed00a52234fc2af0592d22da71
evb0110 <7149553@gmail.com>
2019-02-22 00:41:23 +0300

    solved and tested

---
+++ __tests__/Team.test.js
@@ -0,0 +1,35 @@
+import Team from '../src/js/Team';
+
+const bowman = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 1,
+  attack: 40,
+  defence: 10,
+};
+
+const magician = {
+  name: 'Маг',
+  type: 'Bowman',
+  health: 100,
+  level: 2,
+  attack: 50,
+  defence: 100
+}
+
+test('testing a team with two characters', () => {
+  const team = new Team(bowman, magician);
+  const arrResult = [];
+  for (let character of team) {
+    arrResult.push(character);
+  }
+
+  expect(arrResult).toEqual([bowman, magician]);
+});
+
+test('testing an empty team, should fail', () => {
+  const makeResult = () =>  new Team();
+  
+  expect(makeResult).toThrow('The team cannot be empty!');
+});
---
+++ src/js/Team.js
@@ -0,0 +1,28 @@
+export default class Team {
+  constructor(...characters) {
+    if (characters.length === 0) {
+      throw new Error('The team cannot be empty!');
+    }
+    this.characters = characters;
+  }
+  [Symbol.iterator]() {
+    let current = 0;
+    const last = this.characters.length;
+
+    return {
+      next: () => {
+        if (current < last) {
+          return {
+            done: false,
+            value: this.characters[current++]
+          }
+        } else {
+          return {
+            done: true
+          }
+        }
+      }
+    }
+  }
+}
+
